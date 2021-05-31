5417669730dc9c7c76719046ad5bae19d4e0f7d7
Kamila Graf <kam.graf0@gmail.com>
2021-05-31 19:46:26 +0200

    initial commit

---
+++ src/ui.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './ui.css';
+
+const App = () => {
+    return (
+        <div>
+            <h2>Test</h2>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
